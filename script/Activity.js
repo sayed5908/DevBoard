48a8323da785938e94ed37a5f4510ce241fbb660
sayed <sayed15-5908@diu.edu.bd>
2025-03-27 01:17:49 +0600

    first page completed

---
+++ script/Activity.js
@@ -0,0 +1,6 @@
+document.getElementById("clearbtn").addEventListener("click", 
+    function(){
+        console.log("helo")
+        
+        document.getElementById("activity-history").innerHTML = "";
+})
---
+++ script/ShopEase.js
@@ -0,0 +1,38 @@
+
+document.getElementById("button1").addEventListener("click", 
+    function(){
+        alert("board updated successfully");
+        document.getElementById("button1").disabled = true;
+
+        const task = document.getElementById("tasknumber").innerText;
+        const convertedTask = parseInt(task);
+
+        document.getElementById("tasknumber").innerText = convertedTask-1;
+
+        const ComTask = document.getElementById("CompletedTask").innerText;
+        convertedComTask = parseInt(ComTask);
+
+        document.getElementById("CompletedTask").innerText = convertedComTask+1;
+
+        const now = new Date();
+        let hours = now.getHours();
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        const seconds = now.getSeconds().toString().padStart(2, '0');
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        
+        hours = hours % 12;
+        hours = hours ? hours : 12;  
+  
+        const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+
+        const history = document.getElementById("activity-history");
+        const p = document.createElement("p");
+        p.classList.add("bg-gray-100", "rounded-lg", "m-5", "px-5", "pb-5", "text-gray-600")
+        p.innerText = `
+        You have Completed The Task FIx Mobile Button Issue at ${timeString}
+        `
+
+        history.appendChild(p)
+
+})
+
